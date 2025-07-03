bb38c8b6705b7ef763480b275ab5e7b03765845c
Manahil <manahil.shahid@stu.th-deg.de>
2025-07-03 11:27:13 +0200

    load the trained model

--- endpoint_host.py
+++ endpoint_host.py
@@ -4,3 +4,10 @@ import joblib
 import pandas as pd
 import numpy as np
 import datetime
+
+app = Flask(__name__)
+
+# Load the trained model
+model = joblib.load(
+    r"C:\Users\manah\OneDrive\Desktop\dps-challenge\AI_Challenge-1\model\rf_model.pkl"
+)
